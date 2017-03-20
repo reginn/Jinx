3e80d29ed5b82174c2c8a33adae2cdc45a16fa3e
reginn <reginn666@gmail.com>
2017-03-20 23:15:43 +0900

    装備しているアイコンを画面左上に表示する処理を追加。 ただしインベントリ表示などの画面暗転時に既存処理が正常に稼働しないバグあり

--- src/main/java/com/rgn/jinx/event/ForgeEvents.java
+++ src/main/java/com/rgn/jinx/event/ForgeEvents.java
@@ -5,15 +5,35 @@ import com.google.common.collect.Lists;
 import com.rgn.jinx.init.JinxMessages;
 import com.rgn.jinx.item.ItemElvenBow;
 import com.rgn.jinx.network.EquipArrowInfoMessage;
+import com.sun.org.apache.regexp.internal.RE;
+import com.sun.org.apache.xerces.internal.impl.dtd.models.CMLeaf;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemAnvilBlock;
 import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
 import java.util.List;
 
@@ -76,4 +96,70 @@ public class ForgeEvents {
 
     }
 
+
+    @SubscribeEvent
+    public void drawEquippedArrowIcon(RenderGameOverlayEvent.Post event) {
+
+        if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) {
+            return ;
+        }
+
+        EntityPlayer player = FMLClientHandler.instance().getClient().thePlayer;
+        ItemStack bow = null;
+        ItemStack arrow = null;
+
+        if (player.getHeldItemMainhand() != null && player.getHeldItemMainhand().getItem() instanceof ItemElvenBow) {
+            bow = player.getHeldItemMainhand();
+        } else if (player.getHeldItemOffhand() != null && player.getHeldItemOffhand().getItem() instanceof  ItemElvenBow) {
+            bow = player.getHeldItemOffhand();
+        } else {
+            return ;
+        }
+
+
+
+        int ammoIndex = ((ItemElvenBow) bow.getItem()).readNBTTagCompoundFromItemStack(bow).first();
+
+        int x = 16;
+        int y = 16;
+
+        List<Pair<ItemStack, Integer>> arrowList = ((ItemElvenBow) bow.getItem()).createAmmoList(player);
+        arrow = ((ItemElvenBow) bow.getItem()).getEquipAmmo(bow, arrowList);
+
+        RenderItem renderItem = FMLClientHandler.instance().getClient().getRenderItem();
+
+        GL11.glPushMatrix();
+        GlStateManager.translate(0.0F, 0.0F, 32.0F);
+        renderItem.zLevel = 200.0F;
+        net.minecraft.client.gui.FontRenderer font = null;
+        if (arrow != null) font = arrow.getItem().getFontRenderer(arrow);
+        if (font == null) font = FMLClientHandler.instance().getClient().fontRendererObj;
+        renderItem.renderItemAndEffectIntoGUI(arrow, x, y);
+        renderItem.renderItemOverlayIntoGUI(font, arrow, x, y, "a");
+        renderItem.zLevel = 0.0F;
+        GL11.glPopMatrix();
+    }
+//
+//    protected void renderItemModelIntoGUI(ItemStack stack, int x, int y, IBakedModel bakedmodel)
+//    {
+//        TextureManager textureManager = FMLClientHandler.instance().getClient().getTextureManager();
+//        GlStateManager.pushMatrix();
+//        textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+//        textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
+//        GlStateManager.enableRescaleNormal();
+//        GlStateManager.enableAlpha();
+//        GlStateManager.alphaFunc(516, 0.1F);
+//        GlStateManager.enableBlend();
+//        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+//        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+//        //this.setupGuiTransform(x, y, bakedmodel.isGui3d());
+//        bakedmodel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(bakedmodel, ItemCameraTransforms.TransformType.GUI, false);
+//        FMLClientHandler.instance().getClient().getRenderItem().renderItem(stack, bakedmodel);
+//        GlStateManager.disableAlpha();
+//        GlStateManager.disableRescaleNormal();
+//        GlStateManager.disableLighting();
+//        GlStateManager.popMatrix();
+//        textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+//        textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
+//    }
 }
